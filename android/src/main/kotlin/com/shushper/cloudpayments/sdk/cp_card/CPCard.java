97ab4164c6564a89bac77eb1f38ae89590068268
eevee <vosferal@gmail.com>
2024-05-26 11:56:14 +0300

    update pubkey

--- android/src/main/kotlin/com/shushper/cloudpayments/sdk/cp_card/CPCard.java
+++ android/src/main/kotlin/com/shushper/cloudpayments/sdk/cp_card/CPCard.java
@@ -220,20 +220,14 @@ public class CPCard {
         String s = date + "/" + order_uuid + "/" + cardNumber + "/" + cardCvv + "/" + exp + "/" + sberOrderId;
 
         byte[] bytes = s.getBytes("ASCII");
+
         Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding");
-        SecureRandom random = new SecureRandom();
-        cipher.init(Cipher.ENCRYPT_MODE, getRSAKey(), random);
+
+        cipher.init(Cipher.ENCRYPT_MODE, getRSAKey());
         byte[] crypto = cipher.doFinal(bytes);
-        String crypto64 = "02" +
-                shortNumber +
-                exp + KEY_VERSION() +
-                Base64.encodeToString(crypto, Base64.DEFAULT);
-        String[] cr_array = crypto64.split("\n");
-        crypto64 = "";
-        for (int i = 0; i < cr_array.length; i++) {
-            crypto64 += cr_array[i];
-        }
-        return crypto64;
+
+        return Base64.encodeToString(crypto, Base64.DEFAULT);
+
     }
 
     /**
@@ -254,8 +248,12 @@ public class CPCard {
         byte[] bytes = cardCvv.getBytes("ASCII");
         Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
         SecureRandom random = new SecureRandom();
+
         cipher.init(Cipher.ENCRYPT_MODE, getRSAKey(), random);
+
         byte[] crypto = cipher.doFinal(bytes);
+
+
         String crypto64 = "02" +
                 KEY_VERSION() +
                 Base64.encodeToString(crypto, Base64.DEFAULT);
